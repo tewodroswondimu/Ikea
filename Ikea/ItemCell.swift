0d1881beab0fe79b98c9a915449898bbf9571253
Tewodros Wondimu <tewodros.wr@gmail.com>
2017-10-30 21:34:14 +0200

    Added user interface buttons

---
+++ Ikea/ItemCell.swift
@@ -0,0 +1,14 @@
+//
+//  ItemCell.swift
+//  Ikea
+//
+//  Created by Tewodros Wondimu on 10/30/17.
+//  Copyright © 2017 Tewodros Wondimu. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UICollectionViewCell {
+    
+    @IBOutlet weak var itemLabel: UILabel!
+}
--- Ikea/ViewController.swift
+++ Ikea/ViewController.swift
@@ -9,8 +9,9 @@
 import UIKit
 import ARKit
 
-class ViewController: UIViewController  {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource  {
     let configuration = ARWorldTrackingConfiguration()
+    let itemsArray: [String] = ["Cup", "Vase", "Boxing", "Table"]
     
     @IBOutlet weak var itemCollectionView: UICollectionView!
     
@@ -20,6 +21,8 @@ class ViewController: UIViewController  {
         super.viewDidLoad()
         self.sceneView.session.run(configuration)
         self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
+        self.itemCollectionView.dataSource = self
+        self.itemCollectionView.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -28,6 +31,36 @@ class ViewController: UIViewController  {
         // Dispose of any resources that can be recreated.
     }
 
-
+    // returns how many cells that the collection view will display
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return itemsArray.count
+    }
+    
+    // the contents of the collection view cell
+    // a data source function every single view in the collection
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        // deque returns a cell type based on the identifier item
+        // indexpath is concerned with which cell we're configuring
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "item", for: indexPath) as! ItemCell
+        
+        // Sets the label of the uicollection view to the name in the items array
+        cell.itemLabel.text = self.itemsArray[indexPath.row]
+        
+        return cell
+    }
+    
+    // When ever a button is pressed you change the background to the color green
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at: indexPath)
+        cell?.backgroundColor = UIColor.green
+    }
+    
+    // Change the color of the collection view back to orange when deslected
+    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at: indexPath)
+        cell?.backgroundColor = UIColor.orange
+    }
+    
 }
 
